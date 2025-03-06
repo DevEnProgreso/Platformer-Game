1d88f571472648c8715e023a9b5ca1af5b724656
Frede <freddyzorio12@gmail.com>
2025-03-05 23:55:47 -0600

    second commit

--- script.js
+++ script.js
@@ -70,7 +70,14 @@ class Platform {
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 }
-
+class CheckPoint {
+  constructor(x, y, z) {
+    this.position = {
+      x,
+      y,
+    };
+  }
+}
 const player = new Player();
 
 const platformPositions = [
@@ -122,6 +129,35 @@ const animate = () => {
       });
     }
   }
+
+  platforms.forEach((platform) => {
+    const collisionDetectionRules = [
+      player.position.y + player.height <= platform.position.y,
+      player.position.y + player.height + player.velocity.y >=
+        platform.position.y,
+      player.position.x >= platform.position.x - player.width / 2,
+      player.position.x <=
+        platform.position.x + platform.width - player.width / 3,
+    ];
+
+    if (collisionDetectionRules.every((rule) => rule)) {
+      player.velocity.y = 0;
+      return;
+    }
+
+    const platformDetectionRules = [
+      player.position.x >= platform.position.x - player.width / 2,
+      player.position.x <=
+        platform.position.x + platform.width - player.width / 3,
+      player.position.y + player.height >= platform.position.y,
+      player.position.y <= platform.position.y + platform.height,
+    ];
+
+    if (platformDetectionRules.every((rule) => rule)) {
+      player.position.y = platform.position.y + player.height;
+      player.velocity.y = gravity;
+    }
+  });
 };
 
 const keys = {
